ece028743a5a9a98d642fdcf631dc33f5d5c46b0
Jérémie Piellard <jeremie.piellard@gmail.com>
2020-10-16 17:46:43 +0200

    Edit intro

--- src/generate-page.ts
+++ src/generate-page.ts
@@ -7,7 +7,7 @@ const data = {
     title: "Sine waves",
     description: "Transformation of a picture into a series of sine waves.",
     introduction: [
-        "This is a simple tool to turn images into a series of sine waves of varying frequency and amplitude.",
+        "This is a simple tool that turns images into a series of sine waves of varying frequency and amplitude.",
         "The result can be exported in the SVG format."
     ],
     githubProjectName: "image-stylization-sines",
